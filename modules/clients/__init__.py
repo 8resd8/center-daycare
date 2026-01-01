d9f7b3e912b22140181007058d9967be4c3f1932
김현우 <dkanfjgwls@naver.com>
2026-01-01 20:06:26 +0900

    refactor: 쓰지않는 내용 제거

--- modules/clients/__init__.py
+++ modules/clients/__init__.py
@@ -1,13 +1,11 @@
 """AI 클라이언트 및 프롬프트 패키지"""
 
 from .ai_client import get_ai_client, AIClient
-from .daily_prompt import get_evaluation_prompt
 from .weekly_prompt import WEEKLY_WRITER_SYSTEM_PROMPT, WEEKLY_WRITER_USER_TEMPLATE
 
 __all__ = [
     'get_ai_client',
     'AIClient',
-    'get_evaluation_prompt',
     'WEEKLY_WRITER_SYSTEM_PROMPT',
     'WEEKLY_WRITER_USER_TEMPLATE'
 ]
